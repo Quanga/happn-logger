3b5a5eca1bf89b41ce97ccb1c576a10dea6c6661
Tim Bewsey <tim@h2s.co.za>
2019-06-11 13:13:01 +0200

    updated log4sj

--- index.js
+++ index.js
@@ -4,7 +4,7 @@
  *
  */
 
-if (typeof global['happn-logger'] == 'undefined') {
+if (typeof global['happn-logger'] === 'undefined') {
   Object.defineProperty(global, 'happn-logger', {
     value: require('./lib/logger'),
     configurable: true
